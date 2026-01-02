4460c75d26a27af5f7f94dd42cd28d274c2a6574
Fahim Ahammed Firoz <fahimahammed.cse@gmail.com>
2026-01-02 15:38:45 +0600

    feat: update seedAdmin script with new admin credentials and enhanced logging

--- src/scripts/seedAdmin.ts
+++ src/scripts/seedAdmin.ts
@@ -3,12 +3,14 @@ import { UserRole } from "../middlewares/auth";
 
 async function seedAdmin() {
     try {
+        console.log("***** Admin Seeding Started....")
         const adminData = {
-            name: "Admin Saheb",
-            email: "admin@admin.com",
+            name: "Admin2 Saheb",
+            email: "admin2@admin.com",
             role: UserRole.ADMIN,
             password: "admin1234"
         }
+        console.log("***** Checking Admin Exist or not")
         // check user exist on db or not
         const existingUser = await prisma.user.findUnique({
             where: {
@@ -28,7 +30,26 @@ async function seedAdmin() {
             body: JSON.stringify(adminData)
         })
 
+
+
+        if (signUpAdmin.ok) {
+            console.log("**** Admin created")
+            await prisma.user.update({
+                where: {
+                    email: adminData.email
+                },
+                data: {
+                    emailVerified: true
+                }
+            })
+
+            console.log("**** Email verification status updated!")
+        }
+        console.log("******* SUCCESS ******")
+
     } catch (error) {
         console.error(error);
     }
 }
+
+seedAdmin()
