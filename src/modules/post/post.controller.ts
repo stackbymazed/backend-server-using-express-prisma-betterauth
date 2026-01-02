7e5e9542eef11e039ab14290cef186b2938432a7
Fahim Ahammed Firoz <fahimahammed.cse@gmail.com>
2026-01-02 16:19:53 +0600

    feat: enhance getAllPost service to support pagination and sorting

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -42,7 +42,15 @@ const getAllPost = async (req: Request, res: Response) => {
 
         const authorId = req.query.authorId as string | undefined
 
-        const result = await postService.getAllPost({ search: searchString, tags, isFeatured, status, authorId })
+        const page = Number(req.query.page ?? 1)
+        const limit = Number(req.query.limit ?? 10)
+
+        const skip = (page - 1) * limit
+
+        const sortBy = req.query.sortBy as string | undefined
+        const sortOrder = req.query.sortOrder as string | undefined
+
+        const result = await postService.getAllPost({ search: searchString, tags, isFeatured, status, authorId, page, limit, skip, sortBy, sortOrder })
         res.status(200).json(result)
     } catch (e) {
         res.status(400).json({
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -17,13 +17,23 @@ const getAllPost = async ({
     tags,
     isFeatured,
     status,
-    authorId
+    authorId,
+    page,
+    limit,
+    skip,
+    sortBy,
+    sortOrder
 }: {
     search: string | undefined,
     tags: string[] | [],
     isFeatured: boolean | undefined,
     status: PostStatus | undefined,
-    authorId: string | undefined
+    authorId: string | undefined,
+    page: number,
+    limit: number,
+    skip: number,
+    sortBy: string | undefined,
+    sortOrder: string | undefined
 }) => {
     const andConditions: PostWhereInput[] = []
 
@@ -78,8 +88,13 @@ const getAllPost = async ({
     }
 
     const allPost = await prisma.post.findMany({
+        take: limit,
+        skip,
         where: {
             AND: andConditions
+        },
+        orderBy: {
+            sortBy: sortOrder
         }
     });
     return allPost;
