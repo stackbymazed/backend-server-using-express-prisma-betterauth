6481ad71c420f325a35b54412e5288256d839726
Nayem783982 <majedulislam223311@gmail.com>
2026-01-29 21:21:25 +0600

    create the comment

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -3,7 +3,8 @@ import { commentService } from "./comment.service";
 
 const createComment = async (req: Request, res: Response) => {
     try {
-        
+        const user = req.user
+        req.body.authorId = user?.id;
         const result = await commentService.createComment(req.body)
         res.status(201).json(result)
     } catch (e) {
--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,14 +1,28 @@
 import { prisma } from "../../lib/prisma";
 
-const createComment = async (payload:{
-    content:string;
-    authorId:string;
-    postId:string;
-    parentId?:string;
+const createComment = async (payload: {
+    content: string;
+    authorId: string;
+    postId: string;
+    parentId?: string;
 }) => {
-    console.log("create a comment",payload);
+    console.log("create a comment", payload);
+
+    prisma.post.findUniqueOrThrow({
+        where: {
+            id: payload.postId
+        }
+    })
+
+    if (payload.parentId) {
+        prisma.comment.findUniqueOrThrow({
+            where: {
+                id: payload.parentId
+            }
+        })
+    }
     return await prisma.comment.create({
-        data:payload
+        data: payload
     })
 }
 
